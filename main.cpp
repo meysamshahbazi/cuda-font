d1927c15c0d50cae6e8915dc95c87a2ef7e89efa
meysam <mysm.shahbazi@gmail.com>
2023-08-07 11:38:06 +0330

    build first project

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "cudaFont.h"
+
+
+
+
+
+using namespace std;
+
+int main()
+{
+
+    cudaFont* m_font_bold = cudaFont::Create("/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf",
+    40.0f); 
+
+    return -1;
+}
+
 
